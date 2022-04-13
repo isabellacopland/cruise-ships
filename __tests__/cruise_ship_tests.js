28561b6ae073cd5ce641c6c30b13126c18fbd77b
isabellacopland <isabella.copland@gmail.com>
2022-04-13 19:53:44 +0100

    set sail added

---
+++ __tests__/cruise_ship_tests.js
@@ -0,0 +1,23 @@
+const Ship = require('../src/cruise_ships.js')
+
+describe('Ship', () => {
+    it('can be instantiated', () => {
+        expect(new Ship()).toBeInstanceOf(Object);
+    });
+
+    it('has a starting port', () => {
+        const ship = new Ship('Dover');
+
+        expect(ship.startingPort).toBe('Dover');
+    });
+});
+
+describe('setSail', () => {
+    it('can set sail', () => {
+        const ship = new Ship('Dover');
+
+        ship.setSail();
+
+        expect(ship.startingPort).toBeFalsy();
+    })
+})
---
+++ src/cruise_ships.js
@@ -0,0 +1,9 @@
+function Ship(startingPort) {
+    this.startingPort = (startingPort)
+};
+
+Ship.prototype.setSail = function () {
+   this.startingPort = null
+};
+
+module.exports = Ship;
