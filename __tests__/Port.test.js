dbe2c2c7b04574a78cc3a63781b8edb2093c24a6
isabellacopland <isabella.copland@gmail.com>
2022-04-19 22:04:21 +0100

    add ships and remove ships from Port

--- __tests__/Port.test.js
+++ __tests__/Port.test.js
@@ -1,4 +1,5 @@
 const Port = require('../src/Port.js');
+const Ship = require('../src/cruise_ships.js')
 
 describe('Port', () => {
     it('can be instantiated', () => {
@@ -11,3 +12,28 @@ describe('Port', () => {
         expect(port.name).toBe('Dover');
     });
 })
+
+describe('addShip', () => {
+    it('can add a ship to the port', () => {
+    const port = new Port('Dover');
+    const ship = {};
+
+    port.addShip(ship);
+
+    expect(port.ships).toContain(ship);
+    });
+});
+
+describe('removeShip', () => {
+    it('can remove the ship from the port', () => {
+    const port = new Port('Dover');
+    const titanic = {};
+    const queenMary = {};
+
+   port.addShip(titanic);
+   port.addShip(queenMary);
+   port.removeShip(queenMary);
+
+   expect(port.ships).toEqual([titanic]);
+    });
+});
--- src/Port.js
+++ src/Port.js
@@ -1,5 +1,15 @@
 function Port(name) {
-    this.name = name;
+    this.name = (name);
+    this.ships = [];
 };
 
-module.exports = Port
+Port.prototype = { addShip(ship) {
+    this.ships.push(ship);
+    },
+
+    removeShip(ship) {
+    this.ships.pop(ship);
+    }
+};
+
+module.exports = Port;
